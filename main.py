cd4c920d9a44e36a05fcffd8726304643a48ef79
Rodrigolppz <rodrigobracens@gmail.com>
2026-05-16 22:41:55 -0300

    commit my-webserver

---
+++ main.py
@@ -0,0 +1,49 @@
+import socket
+import time
+
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 8080
+
+# 1 - Create a Socket Connection
+
+# AF_INET gets my IPV4 and SOCK_STREAM uses TCP Protocol.
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Adding additional settings to change the behavior of the socket
+# This allows this socket to reuse the address
+
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Bind socket to an IPV4
+
+server_socket.bind((SERVER_HOST, SERVER_PORT))
+
+# Make socket listen for incoming requests
+# 5 represents the amount of connections the socket can handle
+
+server_socket.listen(5)
+
+print(f"Listening on port {SERVER_PORT} ...")
+
+# Loop to keep listening for new requests
+
+while True:
+        client_socket, client_address = server_socket.accept()
+        request = client_socket.recv(1500).decode()
+        print(request)
+        headers = request.split('\n')
+        
+        first_header_components = headers[0].split()
+
+        http_method = first_header_components[0]
+        path = first_header_components[1]
+
+        if path == '/':
+            fin = open('index.html')
+            content = fin.read()
+            fin.close()
+
+            response = 'HTTP/1.1 200 OK\n\n' + content
+            client_socket.sendall(response.encode())
+            client_socket.close()
+   
